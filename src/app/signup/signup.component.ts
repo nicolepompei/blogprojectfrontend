848793a07833739fe7a4e459d028337b774884d5
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-09 21:29:38 -0500

    added signup request payload interface, added signup requestpayload to constructor of signup component, added form group and form controls to ngOnInit method signup component, added signup method to signup component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { SignupRequestPayload } from './signup-request.payload';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupRequestPayload: SignupRequestPayload;
+  signupForm: FormGroup;
+  constructor() { 
+    this.signupRequestPayload = {
+      username: '',
+      password: '',
+      email: ''
+    }
+  }
 
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    })
+  }
+
+  signup(){
+    this.signupRequestPayload.username =  this.signupForm.get('username').value;
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
   }
 
 }
