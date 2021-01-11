c86b3a05c32df368d703b43c98e9bf6ebb51c6f0
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-11 13:55:24 -0500

    re-added local storage, added API URL constants

--- src/app/service/authservice.service.ts
+++ src/app/service/authservice.service.ts
@@ -7,6 +7,10 @@ import { LoginRequestPayload } from '../login/login.request.payload';
 import { LoginResponse } from '../login/login.response.payload';
 import { LocalStorageService } from 'ngx-webstorage';
 import { map } from 'rxjs/operators';
+import { API_URL } from '../app.constants';
+
+export const TOKEN = 'token';
+export const AUTHENTICATED_USER = 'authenticatedUser';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +21,11 @@ export class AuthserviceService {
     private localStorage: LocalStorageService) { }
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any>{
-   return this.httpClient.post('http://localhost:8080/api/auth/signup', signupRequestPayload, { responseType: 'text'});
+   return this.httpClient.post(`${API_URL}/api/auth/signup`, signupRequestPayload, { responseType: 'text'});
   }
 
   login(loginRequestPayload: LoginRequestPayload): Observable<boolean>{
-    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', loginRequestPayload)
+    return this.httpClient.post<LoginResponse>(`${API_URL}/api/auth/login`, loginRequestPayload)
       .pipe(map(data => {
         this.localStorage.store('authenticationToken', data.authenticationToken);
         this.localStorage.store('username', data.username);
