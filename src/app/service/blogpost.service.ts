fb96feb1437ebfacbaefd732f18c61067df525aa
Christian Pirhalla <chrispir@udel.edu>
2021-01-14 14:34:03 -0500

    cleaned up, just needs tagging and redirecting now

--- src/app/service/blogpost.service.ts
+++ src/app/service/blogpost.service.ts
@@ -3,8 +3,6 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { API_URL } from '../app.constants';
 import { PostRequestPayload } from '../createblogpost/post-request.payload';
 import { Observable } from 'rxjs';
-import { AuthserviceService } from './authservice.service';
-
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +10,12 @@ import { AuthserviceService } from './authservice.service';
 export class BlogpostService {
 
 
-  constructor(private httpClient: HttpClient,
-    private authService: AuthserviceService) { }
+  constructor(private httpClient: HttpClient) { }
 
 
   blogpostConnector(postRequest: PostRequestPayload): Observable<any> {
     console.log("sending to backend");
     let headers = new HttpHeaders();
-    let token: string = this.authService.getJwtToken();
-    console.log("token: " + token);
-    headers = headers.set('Authorization', token);
     let response = this.httpClient.post(`${API_URL}/posts`, postRequest, { responseType: 'text'})
     response.subscribe(resp => console.log(resp));
     return response;
